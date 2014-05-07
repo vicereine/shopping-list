2846e883114834f2cd8c0cd111ceebe6ba57bb2b
vicereine <lauraseabry@gmail.com>
2014-05-07 07:57:20 -0400

    Level 2 revisions

--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
+$(document).ready(function(){
 
+    $('#item').keyup(function(event) {
+        if (event.keyCode == 13) {
+        var value = $(this).val();
+         $('#list').append('<li><input type="checkbox" name="food" value="New Value">' + value + '</li>');
+        }
+        
+    })
+    $('#add').on('click', function() {
+      alert('All Keyed Up')
+    })
+
+    
+
+
+
+
+
+
+
+
+});
 
